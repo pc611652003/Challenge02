08a2cb5c4f10ce650ffd1c3f4e9d081ec9810ab9
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2022-02-26 01:30:54 -0700

    add more project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,83 @@
 var workExamplesDetails = [
     {
+        altName: "Module-1",
         name: "Run-Buddy",
         imageLink: "./assets/images/run-buddy-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/run-buddy/",
         RepositoryLink: "https://github.com/pc611652003/run-buddy/"
     }, 
     {
+        altName: "Challenge-1",
         name: "Code Refactor",
         imageLink: "./assets/images/challenge01-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/Challenge01/",
         RepositoryLink: "https://github.com/pc611652003/Challenge01/"
     },
     {
+        altName: "Challenge-3",
         name: "Password Generator",
         imageLink: "./assets/images/password-generator-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/Password-Generator/",
         RepositoryLink: "https://github.com/pc611652003/Password-Generator/"
     },
     {
+        altName: "Challenge-4",
         name: "Code Quiz",
         imageLink: "./assets/images/code-quiz-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/Code-Quiz/",
         RepositoryLink: "https://github.com/pc611652003/Code-Quiz/"
     },
     {
+        altName: "Challenge-5",
         name: "Work Day Scheduler",
         imageLink: "./assets/images/work-day-scheduler-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/Work-Day-Scheduler/",
         RepositoryLink: "https://github.com/pc611652003/Work-Day-Scheduler/"
     },
     {
+        altName: "Challenge-6",
         name: "Weather Dashboard",
         imageLink: "./assets/images/weather-dashboard-page.PNG",
         ApplicationLink: "https://pc611652003.github.io/Weather-Dashboard/",
         RepositoryLink: "https://github.com/pc611652003/Weather-Dashboard/"
     },
     {
+        altName: "Project-1",
         name: "Travel Agent",
         imageLink: "./assets/images/travel-agent-page.PNG",
         ApplicationLink: "https://maxh1231.github.io/travel-agent/",
         RepositoryLink: "https://github.com/pc611652003/travel-agent/"
     },
     {
+        altName: "Challenge-11",
+        name: "Note Taker",
+        imageLink: "./assets/images/Note-Taker-page.PNG",
+        ApplicationLink: "https://thawing-depths-69202.herokuapp.com/notes",
+        RepositoryLink: "https://github.com/pc611652003/Note-Taker"
+    },
+    {
+        altName: "Challenge-14",
+        name: "Tech Blog",
+        imageLink: "./assets/images/Tech-Blog-page.PNG",
+        ApplicationLink: "https://frozen-retreat-94147.herokuapp.com/",
+        RepositoryLink: "https://github.com/pc611652003/Tech-Blog"
+    },
+    {
+        altName: "Project-2",
         name: "Allergy-Finder",
         imageLink: "./assets/images/allergy-finder-page.PNG",
         ApplicationLink: "https://whispering-chamber-78611.herokuapp.com/",
         RepositoryLink: "https://github.com/pc611652003/Allergy-Finder"
     },
     {
+        altName: "Challenge-19",
+        name: "Budget Tracker",
+        imageLink: "./assets/images/Budget-Tracker-page.PNG",
+        ApplicationLink: "https://vast-wildwood-92684.herokuapp.com/",
+        RepositoryLink: "https://github.com/pc611652003/Budget-Tracker"
+    },
+    {
+        altName: "Project-3",
         name: "FEEDback",
         imageLink: "./assets/images/FEEDback-page.PNG",
         ApplicationLink: "https://polar-mountain-81832.herokuapp.com/",
